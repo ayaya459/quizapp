16242a11b4cadd0dfbb65f0f89e3e09091557bfe
ayaya459 <ayaya459@gmail.com>
2020-01-30 08:44:17 +0100

    first commit

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="question-box-container">
+    <b-jumbotron header="" lead="Bootstrap v4 Components for Vue.js 2">
+      <template v-slot:lead>
+        {{ currentQuestion.question }}
+      </template>
+
+      <hr class="my-4" />
+
+      <b-list-group>
+        <b-list-group-item
+        v-for="(answer,index) in answers"
+        :key="index">
+        {{ asnwers }}
+        </b-list-group-item>
+      </b-list-group>
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button @click="next" variant="success" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    currentQuestion: Object,
+    next: Function
+  },
+  computed: {
+    answers() {
+      let answers = [...this.currentQuestion.incorrect_answers];
+      answers.push(this.currentQuestion.correct_answer);
+      return answers;
+    }
+  }
+};
+</script>
